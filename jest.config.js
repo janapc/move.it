4b7472e10997da01297836a1c728c07bec9ca386
janaina <janaina@kiina.ai>
2021-03-10 10:38:04 -0300

    ci : change configuration of tests e2e

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,8 @@ module.exports = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/tests/fileMock.ts",
-    "\\.(css|less)$": "<rootDir>/tests/styleMock.ts"
+      "<rootDir>/tests/fileMock.js",
+    "\\.(css|less)$": "<rootDir>/tests/styleMock.js"
   },
   testMatch: ["<rootDir>/tests/unit/**/*.spec.{j,t}s?(x)"]
 };
